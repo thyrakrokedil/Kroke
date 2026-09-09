fae6c7ec2e9d5f16b34b6891065b10dd8c2d0a08
Thyra Nyström <thyrnyst100@student.kau.se>
2026-09-09 09:06:50 +0200

    Added taxonomy

--- krokedil-kunskapsbank.php
+++ krokedil-kunskapsbank.php
@@ -51,6 +51,39 @@ function thyra_reg_post_type() {
 
 }
 
+//Registrerar taxonomin
+add_action( 'init', 'thyra_reg_taxonomy' );
+function thyra_reg_taxonomy() {
+    $args = array(
+        'labels' => array(
+            'name'                  => 'Supportkategorier',
+            'singular_name'         => 'Supportkategori',
+            'menu_name'             => 'Kategorier',
+            'all_items'             => 'Alla kategorier',
+            'add_new_item'          => 'Lägg till ny kategori',
+            'new_item_name'         => 'Nytt kategorinamn',
+            'edit_item'             => 'Redigera kategori',
+            'view_item'             => 'Visa kategori',
+            'update_item'           => 'Uppdatera kategori',
+            'search_items'          => 'Sök kategorier',
+            'not_found'             => 'Inga kategorier hittades',
+            'filter_by_item'        => 'Filtrera efter kategori',
+            'items_list'            => 'Lista över kategorier',
+            'back_to_items'         => '← Tillbaka till kategorier',
+            'most_used'             => 'Mest använda',
+
+        ),
+        'hierarchical'      => true,
+        'public'            => true,
+        'show_in_rest'      => true,
+        'show_admin_column' => true,
+        'rewrite'           => array( 'slug' => 'supportkategori' ),
+    );
+   register_taxonomy( 'thyra_supportkategori', 'thyra_supportarende', $args );
+
+
+}
+
 
 
 
